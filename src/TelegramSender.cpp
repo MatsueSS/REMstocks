68a5b400dcc1866def17415f49b75996472ff6ff
MatsueSS <newmailnoways199@gmail.com>
2025-06-01 19:53:30 +0300

    Added send msg.

--- src/TelegramSender.cpp
+++ src/TelegramSender.cpp
@@ -1,6 +1,7 @@
 #include "TelegramSender.h"
 
 #include <fstream>
+#include <curl/curl.h>
 
 TelegramSender::TelegramSender()
 {
@@ -21,3 +22,29 @@ void TelegramSender::destroy()
     delete instance;
 }
 
+std::string TelegramSender::query(std::string id, type_msg type, std::string data)
+{
+    std::string response = "";
+    std::string url = "https://api.telegram.org/bot" + token;
+    switch(type){
+        case type_msg::read:
+        {
+            url += "/getUpdates";
+        }
+        case type_msg::send:
+        {
+            url += "/sendMessage";
+            CURL* curl = curl_easy_init();
+            if(curl){
+                char *encoding = curl_easy_escape(curl, data.c_str(), data.length());
+                std::string post_fileds = "chat_id="+id+"&text="+encoding;
+                curl_free(encoding);
+                curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+                curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_fileds.c_str());
+                CURLcode res = curl_easy_perform(curl);
+                curl_easy_cleanup(curl);
+            }
+        }
+    }
+    return response;
+}
