94a45119895b63bf31dc95b4816f05002a866ea5
MatsueSS <newmailnoways199@gmail.com>
2025-06-01 21:29:38 +0300

    Added telegram user.

---
+++ include/TelegramUser.h
@@ -0,0 +1,25 @@
+#ifndef _TELEGRAM_USER_H_
+#define _TELEGRAM_USER_H_
+
+#include <string>
+
+class Observer{
+public:
+    Observer(std::string id);
+
+    virtual void notify(std::string) = 0;
+
+    virtual ~Observer() = default;
+protected:
+    std::string id;
+};
+
+class TelegramUser : public Observer{
+public:
+    TelegramUser(std::string id);
+
+    void notify(std::string) override;
+
+};
+
+#endif //_TELEGRAM_USER_H_
---
+++ src/TelegramUser.cpp
@@ -0,0 +1,15 @@
+#include "TelegramUser.h"
+
+#include <thread>
+
+#include "TelegramSender.h"
+
+Observer::Observer(std::string id) : id(id) {}
+
+TelegramUser::TelegramUser(std::string id) : Observer(std::move(id)) {}
+
+void TelegramUser::notify(std::string sale)
+{
+    auto ptr = TelegramSender::get_instance();
+    ptr->call(id, type_msg::send, sale);
+}
